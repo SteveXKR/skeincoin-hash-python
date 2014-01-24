a94d4aee22e6d2731b536fb8b5991437bce52d54
iamasupernova <tristan.tierney@gmail.com>
2014-01-23 21:58:26 -0500

    change to skein???

--- quark.c
+++ quark.c
@@ -32,11 +32,6 @@
 #include <stdint.h>
 #include <string.h>
 #include <stdio.h>
-#include "sha3/sph_blake.h"
-#include "sha3/sph_bmw.h"
-#include "sha3/sph_groestl.h"
-#include "sha3/sph_jh.h"
-#include "sha3/sph_keccak.h"
 #include "sha3/sph_skein.h"
 
 
@@ -108,11 +103,6 @@ be32dec_vect(uint32_t *dst, const unsigned char *src, size_t len)
 
 void quark_hash(const char* input, char* output)
 {
-    sph_blake512_context     ctx_blake;
-    sph_bmw512_context       ctx_bmw;
-    sph_groestl512_context   ctx_groestl;
-    sph_jh512_context        ctx_jh;
-    sph_keccak512_context    ctx_keccak;
     sph_skein512_context     ctx_skein;
     static unsigned char pblank[1];
 
@@ -121,74 +111,9 @@ void quark_hash(const char* input, char* output)
 
     uint32_t hashA[16], hashB[16];
 
-
-
-    sph_blake512_init(&ctx_blake);
-    sph_blake512 (&ctx_blake, input, 80);
-    sph_blake512_close (&ctx_blake, hashA);	 //0
-
-
-    sph_bmw512_init(&ctx_bmw);
-    sph_bmw512 (&ctx_bmw, hashA, 64);    //0
-    sph_bmw512_close(&ctx_bmw, hashB);   //1
-
-
-    if ((hashB[0] & mask) != zero)   //1
-    {
-        sph_groestl512_init(&ctx_groestl);
-        sph_groestl512 (&ctx_groestl, hashB, 64); //1
-        sph_groestl512_close(&ctx_groestl, hashA); //2
-    }
-    else
-    {
-        sph_skein512_init(&ctx_skein);
-        sph_skein512 (&ctx_skein, hashB, 64); //1
-        sph_skein512_close(&ctx_skein, hashA); //2
-    }
-
-
-    sph_groestl512_init(&ctx_groestl);
-    sph_groestl512 (&ctx_groestl, hashA, 64); //2
-    sph_groestl512_close(&ctx_groestl, hashB); //3
-
-    sph_jh512_init(&ctx_jh);
-    sph_jh512 (&ctx_jh, hashB, 64); //3
-    sph_jh512_close(&ctx_jh, hashA); //4
-
-    if ((hashA[0] & mask) != zero) //4
-    {
-        sph_blake512_init(&ctx_blake);
-        sph_blake512 (&ctx_blake, hashA, 64); //
-        sph_blake512_close(&ctx_blake, hashB); //5
-    }
-    else
-    {
-        sph_bmw512_init(&ctx_bmw);
-        sph_bmw512 (&ctx_bmw, hashA, 64); //4
-        sph_bmw512_close(&ctx_bmw, hashB);   //5
-    }
-
-    sph_keccak512_init(&ctx_keccak);
-    sph_keccak512 (&ctx_keccak,hashB, 64); //5
-    sph_keccak512_close(&ctx_keccak, hashA); //6
-
     sph_skein512_init(&ctx_skein);
     sph_skein512 (&ctx_skein, hashA, 64); //6
     sph_skein512_close(&ctx_skein, hashB); //7
-
-    if ((hashB[0] & mask) != zero) //7
-    {
-        sph_keccak512_init(&ctx_keccak);
-        sph_keccak512 (&ctx_keccak, hashB, 64); //
-        sph_keccak512_close(&ctx_keccak, hashA); //8
-    }
-    else
-    {
-        sph_jh512_init(&ctx_jh);
-        sph_jh512 (&ctx_jh, hashB, 64); //7
-        sph_jh512_close(&ctx_jh, hashA); //8
-    }
-
 
 
 	memcpy(output, hashA, 32);
--- setup.py
+++ setup.py
@@ -3,11 +3,6 @@ from distutils.core import setup, Extension
 quark_hash_module = Extension('quark_hash',
                                sources = ['quarkmodule.c',
                                           'quark.c',
-										  'sha3/blake.c',
-										  'sha3/bmw.c',
-										  'sha3/groestl.c',
-										  'sha3/jh.c',
-										  'sha3/keccak.c',
 										  'sha3/skein.c'],
                                include_dirs=['.', './sha3'])
 
