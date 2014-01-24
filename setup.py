5a796d3c8832cf29526230dec314d8986465b85f
iamasupernova <tristan.tierney@gmail.com>
2014-01-24 04:05:09 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ quark_hash_module = Extension('quark_hash',
 										  'sha3/skein.c'],
                                include_dirs=['.', './sha3'])
 
-setup (name = 'quark_hashs',
+setup (name = 'quark_hash',
        version = '1.0',
        description = 'Bindings for scrypt proof of work used by Quarkcoin',
        ext_modules = [quark_hash_module])
